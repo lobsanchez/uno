cbc1b2d771659e98c14bb1e04389d52a561a623f
Oscar Bueno <lobsanchez@hotmail.com>
2024-02-06 19:05:20 +0100

    prueba de mierda

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html, php, js}",
+    "./src/**/*",
+    "./**/*.{html, php, js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
